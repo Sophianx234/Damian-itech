bcf83b217c01311740c0e3fb63fea4a75c6f1536
sophianx234 <dx4336969@gmail.com>
2026-07-10 14:00:00 +0000

    chore: add framer-motion and update gadget images

--- src/components/TrendingGadgets/TrendingGadgets.tsx
+++ src/components/TrendingGadgets/TrendingGadgets.tsx
@@ -6,14 +6,14 @@ import Autoplay from 'embla-carousel-autoplay';
 import styles from './TrendingGadgets.module.css';
 
 const devices = [
-  { id: 1, name: 'PlayStation 5', image: 'imgs/gadgets/t-1.jpeg' },
-  { id: 2, name: 'Smart TV 4K', image: 'imgs/gadgets/tv-1.jpeg' },
-  { id: 3, name: 'iPhone 15 Pro', image: 'imgs/gadgets/iphone-1.jpeg' },
-  { id: 4, name: 'VR Headset', image: 'imgs/gadgets/vr-1.jpeg' },
-  { id: 5, name: 'Nintendo Switch', image: 'imgs/gadgets/n-1.jpeg' },
-  { id: 6, name: 'MacBook Pro', image: 'imgs/gadgets/mac-1.jpeg' },
-  { id: 7, name: 'Xbox Series X', image: 'imgs/gadgets/xbox-1.jpeg' },
-  { id: 8, name: 'iPad Air', image: 'imgs/gadgets/ipad-1.jpeg' }
+  { id: 1, name: 'PlayStation 5', image: 'imgs/gadgets/p-2.png' },
+  { id: 2, name: 'Smart TV 4K', image: 'imgs/gadgets/tv-2.png' },
+  { id: 3, name: 'iPhone 15 Pro', image: 'imgs/gadgets/iphone-2.png' },
+  { id: 4, name: 'VR Headset', image: 'imgs/gadgets/vr-2.png' },
+  { id: 5, name: 'Nintendo Switch', image: 'imgs/gadgets/n-2.png' },
+  { id: 6, name: 'MacBook Pro', image: 'imgs/gadgets/mac-2.png' },
+  { id: 7, name: 'Xbox Series X', image: 'imgs/gadgets/xbox-2.png' },
+  { id: 8, name: 'iPad Air', image: 'imgs/gadgets/ipad-2.png' }
 ];
 
 const TrendingGadgets = () => {
